7db8ae6310d5289500be56456e15f19d49ce8295
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-05-31 22:06:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,14 +3,15 @@ class Application
   @@items = []
 
   def call(env)
-    resp = Rack:Response.new
+    resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find { |i| i.name == item_name }
       if item
-        resp.write item.price
+        resp.write "#{item.price}"
+        resp.status = 200
       else
         resp.write "Item not found"
         resp.status = 400
@@ -20,6 +21,6 @@ class Application
       resp.status = 404
     end
 
-      resp.finish
-    end
+    resp.finish
   end
+end 
